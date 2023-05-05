7e71fa415d7dee78680d0e08707d260acb54f134
Acci Pigna <acci@derbolina>
2023-05-05 10:46:49 +0200

    Better parking

--- sim/parkman.py
+++ sim/parkman.py
@@ -64,90 +64,59 @@ class Maneuvers():
         # FIRST PART
         print('FIRST PART - START - turn right and go backwards')
         car.drive_angle(self.park_pl_turn_right)
-        # sleep(self.delay)
         car.drive_speed(self.park_pl_backward)
-        # while car.xLoc<self.park_pl_midpoint_x or \
-        # car.yawLoc>self.park_pl_midpoint_yaw:
-        # !!!while car.distLoc < self.park_pl_midpoint_dist:
-        #    print(f'current x and yaw: {car.xLoc, np.rad2deg(car.yawLoc)}')
-        sleep(5.5)  # keep going
-        # car.drive_speed(0.0)
-        # car.stop(self.park_pl_turn_left)
-        # print(f'pose: {car.xLoc, car.yLoc, car.yawLoc, car.distLoc}')
+        sleep(5.5)# keep going
         print('FIRST PART - END')
-
-        # sleep(self.delay)
         car.reset_rel_pose()
-
+        
         # SECOND PART
         print('SECOND PART - START - turn left and go backwards')
         car.drive_angle(self.park_pl_turn_left)
-        # sleep(self.delay)
         car.drive_speed(self.park_pl_backward)
-        # while car.xLoc<self.park_pl_midpoint_x*2.0 or car.yawLoc<0.0:
-        # !!!while car.distLoc < self.park_pl_midpoint_dist:
-        sleep(4.3)  # keep going
-        # car.drive_speed(0.0)
+        sleep(4.5)# keep going
         car.stop()
-        # print(f'pose: {car.xLoc, car.yLoc, car.yawLoc, car.distLoc}')
         print('SECOND PART - END')
-
         sleep(self.delay)
         car.reset_rel_pose()
-        # pspot_back = car.yLoc
+
 
         # THIRD PART
         print('THIRD PART - START - go to the center of the parking spot')
         car.drive_angle(0.0)
         car.drive_speed(self.park_pl_forward)
-        # while car.yLoc - pspot_back < 0.3*self.park_pl_length:
-        # !!!while car.distLoc < 0.2:
-        sleep(0.5)  # keep going
-        # car.drive_speed(0.0)
+        sleep(1.4)# keep going
         car.stop()
-        # print(f'pose: {car.xLoc, car.yLoc, car.yawLoc, car.distLoc}')
         print('THIRD PART - END')
-
         print("*** END OF THE PARKING MANEUVER ***")
         sleep(self.park_wait_time)
         car.reset_rel_pose()
 
         # FOURTH PART
-        print('FOURTH PART - START - go back up to the end of the \
-parking spot')
+        print('FOURTH PART - START - go back up to the end of the parking spot')
         car.drive_speed(self.park_pl_backward)
-        # while car.yLoc - pspot_back > 0.0:
-        # !!!while car.distLoc < 0.2:
-        sleep(0.5)  # keep going
-        # car.drive_speed(0.0)
+        sleep(1.4)# keep going
         car.stop()
-        # print(f'pose: {car.xLoc, car.yLoc, car.yawLoc, car.distLoc}')
         print('FOURTH PART - END')
-
         sleep(self.delay)
         car.reset_rel_pose()
-
+        
         # FIFTH PART
         print('FIFTH PART - START - turn left and go forwards')
         car.drive_angle(self.park_pl_turn_left)
         car.drive_speed(self.park_pl_forward)
-        sleep(4.3)  # keep going
+        sleep(4.5)# keep going
         print('FIFTH PART - END')
-
-        # sleep(self.delay)
         car.reset_rel_pose()
 
         # SIXTH PART
         print('SIXTH PART - START - turn right and go forwards')
         car.drive_angle(self.park_pl_turn_right)
-
         car.drive_speed(self.park_pl_forward)
-
-        sleep(5.5)  # keep going
+        sleep(5.3)# keep going
         car.drive_angle(0.0)
         car.stop()
         print('SIXTH PART - END')
-
+        
     def perpendicular_parking(self, car, way_exit='right'):
         # FIRST PART
         print('FIRST PART - START - turn right and go backwards')
